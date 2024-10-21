f03cfa402cd0636377d5351e1488b52f47256b7b
Marco Rabozzi <marco.rabozzi@huxelerate.it>
2024-10-21 10:38:56 +0200

    refactor errorHandler code

--- autosar/util/errorHandler.py
+++ autosar/util/errorHandler.py
@@ -3,17 +3,21 @@ import traceback
 
 already_shown_errors = set()
 
+def __printExceptionWarning(e: Exception):
+    stack_trace = "".join(traceback.format_stack())
+    exception = f"WARNING - ignoring {type(e).__name__} '{e}' in:\n\r{stack_trace}"
+    if not exception in already_shown_errors:
+        print(exception)
+    else:
+        already_shown_errors.add(exception)
+
+
 def handleNotImplementedError(error: str):
     if os.getenv("AUTOSAR_IGNORE_NOT_IMPLEMENTED_ERROR") == "1":
         try: 
             raise NotImplementedError(error)
         except NotImplementedError as e:
-            stack_trace = "".join(traceback.format_stack())
-            exception = f"WARNING - ignoring NotImplementedError '{e}' in:\n\r{stack_trace}"
-            if not exception in already_shown_errors:
-                print(exception)
-            else:
-                already_shown_errors.add(exception)
+            __printExceptionWarning(e)
     else:
         raise NotImplementedError(error)
 
@@ -22,11 +26,6 @@ def handleValueError(error: str):
         try: 
             raise ValueError(error)
         except ValueError as e:
-            stack_trace = "".join(traceback.format_stack())
-            exception = f"WARNING - ignoring ValueError '{e}' in:\n\r{stack_trace}"
-            if not exception in already_shown_errors:
-                print(exception)
-            else:
-                already_shown_errors.add(exception)
+            __printExceptionWarning(e)
     else:
         raise ValueError(error)
