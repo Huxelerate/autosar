69dc799c3029ee28bcea58859d1adc2815013d6e
Andrea Damiani <1655566+andredami@users.noreply.github.com>
2024-10-21 17:54:11 +0200

    Extend ParameterComSpec generation from dictionary

--- autosar/port.py
+++ autosar/port.py
@@ -139,7 +139,8 @@ class Port(Element):
             if (parameterName is None) and (len(portInterface.parameters)==1):
                 parameterName = portInterface.parameters[0].name
             initValue = comspec.get('initValue', None)
-            return ParameterComSpec(parameterName, initValue)
+            initValueRef = comspec.get('initValueRef', None)
+            return ParameterComSpec(parameterName, initValue, initValueRef)
         elif isinstance(portInterface, autosar.portinterface.NvDataInterface):
             nvDataName = None
             if 'nvData' in comspec: nvDataName=str(comspec['nvData'])
